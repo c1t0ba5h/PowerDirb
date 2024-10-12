b37e978d567358193c964d4c1615e972dc353a6d
c1t0 <williamvivasdev@gmail.com>
2024-10-12 13:45:15 -0400

    added error condition if the wordlist does'nt come

--- powerdirb.ps1
+++ powerdirb.ps1
@@ -35,21 +35,27 @@ try {
     # Add a pause to keep the console open
     Write-Host "`nLaunching PowerDirb Scan..." -ForegroundColor Green
     Start-Sleep -Seconds 1  # Sleep for one second for style.
-    #Get target/wordlist
+
+    # Get target and wordlist URL
     $target = Read-Host "What is your target?"
     $url = Read-Host "What list do you want to use?"
     Write-Host "`nResults:" -ForegroundColor Green
     Write-Host "----------------------------------------------------" -ForegroundColor Green
 
-    # Fetch the content from the external server
-    $response = Invoke-WebRequest -Uri $url -UseBasicParsing
-    $wordList = $response.Content -split "`r?`n"
+    # Attempt to fetch the wordlist
+    try {
+        $response = Invoke-WebRequest -Uri $url -UseBasicParsing -ErrorAction Stop
+        $wordList = $response.Content -split "`r?`n"
+    } catch {
+        Write-Host "Could not retrieve the wordlist: $($_.Exception.Message)" -ForegroundColor Red
+        return  # Exit the script if wordlist retrieval fails
+    }
 
     # Iterate over each word in the list
     foreach ($word in $wordList) {
         if (-not [string]::IsNullOrWhiteSpace($word)) {
-            $full = $target + "/" + $word
-            #Make the web request if the web requests are 200/405/302 print them with diffrent colors
+            $full = "$target/$word"
+            # Make the web request if the response is 200/405/302, print them with different colors
             try {
                 $response = Invoke-WebRequest -Uri $full -Method Get -ErrorAction Stop
 
@@ -62,7 +68,7 @@ try {
                     Write-Host "Found: $full -- response code: $($response.StatusCode)" -ForegroundColor $color
                 }
             } catch {
-                # Don't print 404s to screen
+                # Don't print 404s or other errors to screen
                 continue
             }
         }
@@ -71,5 +77,5 @@ try {
     # This block will always run, even if errors occur
     $currentTime = Get-Date
     Write-Host "----------------------------------------------------" -ForegroundColor Green
-    Write-Host "`nFinished Scan at:" $currentTime -ForegroundColor Green
+    Write-Host "`nFinished Scan at: $currentTime" -ForegroundColor Green
 }
